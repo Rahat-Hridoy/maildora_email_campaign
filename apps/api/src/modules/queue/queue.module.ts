9becfb4abfb794b66463af8b4d003305c2722fb4
Rahat-Hridoy <rahathridoyd2x@gmail.com>
2026-03-12 03:36:37 +0600

    IMP:Real time dashboard for state check using by BullMQ

--- apps/api/src/modules/queue/queue.module.ts
+++ apps/api/src/modules/queue/queue.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bullmq';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { BullBoardModule } from '@bull-board/nestjs';
+import { BullMQAdapter } from '@bull-board/api/bullMQAdapter';
+import { ExpressAdapter } from '@bull-board/express';
 import { PrismaService } from '../../prisma.service';
 import { PRIORITY_QUEUE, BULK_QUEUE, RETRY_QUEUE } from '@maildora/queue';
 
@@ -24,9 +27,16 @@ const defaultJobOptions = {
       }),
       inject: [ConfigService],
     }),
+    BullBoardModule.forRoot({
+      route: '/admin/queues',
+      adapter: ExpressAdapter,
+    }),
     BullModule.registerQueue({ name: PRIORITY_QUEUE }),
     BullModule.registerQueue({ name: BULK_QUEUE }),
     BullModule.registerQueue({ name: RETRY_QUEUE }),
+    BullBoardModule.forFeature({ name: PRIORITY_QUEUE, adapter: BullMQAdapter }),
+    BullBoardModule.forFeature({ name: BULK_QUEUE, adapter: BullMQAdapter }),
+    BullBoardModule.forFeature({ name: RETRY_QUEUE, adapter: BullMQAdapter }),
   ],
   providers: [PrismaService],
   exports: [BullModule],
